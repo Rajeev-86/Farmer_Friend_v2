aebcd21a86ff634a7e1fae10a5ad44c457bc0a6b
Rajeev <rajeev.86dev@gmail.com>
2025-04-04 20:24:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "📦 Downloading models from Google Drive..."
+python models/download_models.py
+
 # Start FastAPI app
+echo "🚀 Starting FastAPI..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
 
