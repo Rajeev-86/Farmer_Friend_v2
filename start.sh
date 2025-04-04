8ccbcd2036f8eb5665e7961fa4a9f43a471e67c7
Rajeev <rajeev.86dev@gmail.com>
2025-04-04 18:27:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,5 +9,5 @@ fi
 pip install --no-cache-dir -r requirements.txt
 
 # Start FastAPI app
-exec uvicorn app.main:app --host 0.0.0.0 --port 7000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
 
